eb57a59ccf3e986a8a22c993c1c211be543916f8
Josue Maldonado <joshmaldo@Josues-MacBook-Pro.local>
2021-06-13 21:28:43 -0500

    added py code for Mars facts

--- scrape_mars.py
+++ scrape_mars.py
@@ -24,3 +24,22 @@ def scrape():
     news_title = mysoup.find('li', class_='slide').find('div', class_='content_title').text
     news_p = mysoup.find('li', class_='slide').find('div', class_='article_teaser_body').text
 
+    # Mars Images define and retrive
+    image_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    # web page gives 404 Error
+
+
+
+    # Mars Facts
+    # Have pandas read any tables on mars facts page
+    facts_url = 'https://space-facts.com/mars/'
+    fact_table = pd.read_html(facts_url)
+    # Filter to table I want to work with
+    fact_df = fact_table[0]
+    # Rename columns
+    fact_df.columns = ["Description", "Mars"]
+    # Remove Index/set new
+    facts = fact_df.set_index("Description")
+    # Convert to html string & clean
+    html_facts = facts.to_html()
+    html_facts = html_facts.replace('\n', '')
